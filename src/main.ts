f84df6373d8ef3d91a2b0e11c164bde5bdb45092
A.David <admaal96@gmail.com>
2024-10-17 21:18:48 +0200

    LaboratorioModulo2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+// Tienes un grupo de 6 amigos y quieres invitarlos a cenar por tu cumpleaños.
+// Solo puedes permitirte invitar a las bebidas, ya que estás un poco ajustado de dinero.
+// Tienes un ticket de cena que cuesta 120 € y en el que ya se incluyen las bebidas por un valor de 18 €.
+// Calcula cuánto tendría que pagar cada comensal para dividir los costos de manera equitativa.
+// Utiliza JavaScript para hacer el cálculo y mostrar el resultado por consola.
+
+// 120 euros menos el precio de las bebidas
+// Cena = 120;
+// Bebidas = 18;
+// Comida = Cena - Bebidas;
+console.log(120 - 18); // 102
+
+// Dividimos 102 euros entre seis personas
+// Amigos = 6;
+// costoPorPersona = Comida / Amigos;
+console.log(102 / 6); // 17
+
+// 17 euros es el resultado
